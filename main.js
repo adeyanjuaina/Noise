f40b249f5f827f7260100650ae936fc2305db68c
adeyanjuaina <51255921+adeyanjuaina@users.noreply.github.com>
2023-03-27 18:53:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+// https://teachablemachine.withgoogle.com/models/ek-qS_yW5/
+function startclassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/ek-qS_yW5/model.json',modelready);
+}
+
+function modelready(){
+
+    classifier.classify(gotresults);
+}
+function gotresults(error,results){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(results);
+       document.getElementById("result_label").innerHTML="I can hear- "+results[0].label;
+       document.getElementById("result_confidence").innerHTML="Accuracy- "+(results[0].confidence*100).toFixed(2)+" %";
+       
+
+       img1=document.getElementById("alien1");
+       img2=document.getElementById("alien2");
+       img3=document.getElementById("alien3");
+       img4=document.getElementById("alien4");
+
+       if(results[0].label=="Clap"){
+        img1.src="aliens-01.gif";
+        img2.src="aliens-02.png";
+        img3.src="aliens-03.png";
+        img4.src="aliens-04.png";
+       }
+
+       else if(results[0].label=="Snap"){
+        img1.src="aliens-01.png";
+        img2.src="aliens-02.gif";
+        img3.src="aliens-03.png";
+        img4.src="aliens-04.png";
+       }
+
+       else if(results[0].label=="Bell"){
+        img1.src="aliens-01.png";
+        img2.src="aliens-02.png";
+        img3.src="aliens-03.gif";
+        img4.src="aliens-04.png";
+       }
+
+       else{
+        img1.src="aliens-01.png";
+        img2.src="aliens-02.png";
+        img3.src="aliens-03.png";
+        img4.src="aliens-04.gif ";
+       }
+    }
+}
